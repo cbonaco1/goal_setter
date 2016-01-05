385ecf1d718b70afa1c42946eb17536bf47d2789
ChristianZach <ChristianZach@gmail.com>
2016-01-05 16:36:16 -0500

    Finish Phsase II

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,5 +1,6 @@
 class GoalsController < ApplicationController
   before_action :ensure_signed_in
+  before_action :ensure_goal_owner, only: [:edit, :update, :destroy]
 
   def index
 
@@ -19,10 +20,41 @@ class GoalsController < ApplicationController
     end
   end
 
+  def edit
+    @goal = Goal.find(params[:id])
+  end
+
+  def update
+    @goal = Goal.find(params[:id])
+    if @goal.update(goal_params)
+      redirect_to goal_url(@goal)
+    else
+      flash.now[:errors] = @goal.errors.full_messages
+      render :edit
+    end
+  end
+
+  def show
+    @goal = Goal.find(params[:id])
+  end
+
+  def destroy
+    @goal = Goal.find(params[:id])
+    @goal.destroy
+    redirect_to user_url(@goal.user)
+  end
+
 
   private
   def goal_params
-    params.require(:goal).permit(:title, :description, :private?)
+    params.require(:goal).permit(:title, :description, :private?, :complete?)
+  end
+
+  def ensure_goal_owner
+    goal = Goal.find(params[:id])
+    unless goal.user == current_user
+      redirect_to goal_url(goal)
+    end
   end
 
 end
--- spec/features/goals_spec.rb
+++ spec/features/goals_spec.rb
@@ -2,25 +2,58 @@ require 'rails_helper'
 
 feature 'Users show page' do
   given(:christian) { User.create!(username: "christian", password: "password") }
+  given(:zach) { User.create!(username: "zach", password: "password") }
 
-  scenario 'Hides private goals from other viewers' do
-    sign_in_as(christian)
-
-    goal = Goal.create!(
+  given!(:christian_private_goal) do
+    Goal.create!(
       user_id: christian.id,
       private?: true,
       title: "My Private Goal Title"
     )
-
-    click_button("Sign Out")
-
-    visit user_url(christian)
-    
-    expect(page).to_not have_content("My Private Goal Title")
-
   end
 
-  scenario 'User can see own private goals'
+  given!(:christian_public_goal) do
+    Goal.create!(
+      user_id: christian.id,
+      private?: false,
+      title: "My Public Goal Title"
+    )
+  end
 
-  scenario 'User can make a new goal from their show page'
+  scenario 'Users can view public goals' do
+    visit user_url(christian)
+    expect(page).to have_content(christian_public_goal.title)
+  end
+
+  scenario 'Hides private goals from other viewers' do
+    visit user_url(christian)
+    expect(page).to_not have_content(christian_private_goal.title)
+  end
+
+  scenario 'User can see own private goals' do
+    sign_in_as(christian)
+    visit user_url(christian)
+    expect(page).to have_content(christian_private_goal.title)
+  end
+
+  scenario 'User can make a new goal from their show page' do
+    sign_in_as(christian)
+    visit user_url(christian)
+    click_link("New Goal")
+    fill_in("Title", with: "New Goal Title")
+    fill_in("Description", with: "Goal Description")
+    page.uncheck('private_checkbox')
+    click_button("Add Goal")
+    expect(page).to have_content("New Goal Title")
+  end
+
+  scenario 'User can edit their goals' do
+    sign_in_as(christian)
+    click_link("My Public Goal Title")
+    click_link("Edit Goal")
+    fill_in("Title", with: "My NEW Public Goal Title")
+    click_button("Update Goal")
+    expect(page).to have_content("My NEW Public Goal Title")
+  end
+
 end
