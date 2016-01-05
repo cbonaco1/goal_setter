4e53534c2005996df08453811bc9d5f0b1103a6b
ChristianZach <ChristianZach@gmail.com>
2016-01-05 11:32:55 -0500

    Finish User login.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,22 @@ class User < ActiveRecord::Base
   validates :password, length: { miniumum: 6, allow_nil: true }
 
   attr_reader :password
+  after_initialize :ensure_session_token
 
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.new(password)
+  end
+
+  def reset_session_token!
+    self.session_token = SecureRandom::urlsafe_base64
+    self.save
+    self.session_token
+  end
+
+  private
+  def ensure_session_token
+    self.session_token ||= SecureRandom::urlsafe_base64
   end
 
 end
